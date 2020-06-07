9023f0c4b24fd45bdf5c9dc1d22950dec248c5a6
Erik Salazar <3salaz.dev@gmail.com>
2020-06-06 22:52:39 -0700

    initial commit

---
+++ pages/homework.js
@@ -0,0 +1,7 @@
+import Home from "."
+
+function Homework() {
+    return <div>Homework</div>
+  }
+  
+  export default Homework
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
+  
   return (
+    
     <div className="container">
       <Head>
         <title>Create Next App</title>
@@ -10,39 +13,44 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome <a href="https://nextjs.org">Devops!</a>
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Lets get started with our<code>Development</code>
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <Link href="/docs">
+            <a className="card docs">
+              <h3>Docs &rarr;</h3>
+              <p>Stay informed with docs from some of your favorite software</p>
+            </a>
+          </Link>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <Link href="/homework">
+            <a className="card homework">
+              <h3>Homework &rarr;</h3>
+              <p>Learn about Next.js in an interactive course with quizzes!</p>
+            </a>
+          </Link>
 
           <a
             href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
+            className="card projects"
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h3>Projects &rarr;</h3>
+            <p>View our collection of student projects and tools</p>
           </a>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className="card"
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Classroom &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Browse past recordings!<br></br>
+              Live weekends 9:00 - 12pm
             </p>
           </a>
         </div>
@@ -58,7 +66,6 @@ export default function Home() {
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
-
       <style jsx>{`
         .container {
           min-height: 100vh;
@@ -204,6 +211,7 @@ export default function Home() {
           box-sizing: border-box;
         }
       `}</style>
+
     </div>
   )
 }
